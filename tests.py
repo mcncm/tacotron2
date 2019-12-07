60990bbd9b351459d899288db51fbce04c07f7b9
mcncm <mcnallychristopherm@gmail.com>
2019-12-07 08:26:32 -0500

    Fixed tests after swapping LR, dR; added cuda flag

--- tests.py
+++ tests.py
@@ -6,6 +6,9 @@ To add a test, just add a new function annotated with the decorator `@test`.
 
 import traceback
 from termcolor import colored
+from collections import defaultdict
+import argparse
+import sys
 
 import torch
 import torch.nn as nn
@@ -25,19 +28,23 @@ hparams = create_hparams()
 
 ### TESTS ###
 @test
-def _test_conv_block():
+def _test_conv_block(test_params):
     """Just feed some numbers through a ConvBlock and make sure it doesn't
     crash.
     """
     input_shape = hparams.input_dims
     # leading 1s for batch size and number of channels (I think? Confirm.)
 
-    input_tensor = torch.randn(1, 1, *input_shape).cuda()
-    cb = ConvBlock(input_shape, 3, 2, [32, 32, 64]).cuda()
+    input_tensor = torch.randn(1, 1, *input_shape)
+    cb = ConvBlock(input_shape, 3, 2, [32, 32, 64])
+    if test_params['cuda']:
+        input_tensor = input_tensor.cuda()
+        cb = cb.cuda()
+
     output = cb(input_tensor)
 
 @test
-def _test_rnn_block():
+def _test_rnn_block(test_params):
     """Just feed some numbers through an RnnBlock and make sure it doesn't
     crash.
     """
@@ -48,15 +55,23 @@ def _test_rnn_block():
     embedding_dim = 128
     batch_size = 32
 
-    input_tensor = torch.randn(batch_size, cnn_channels, LR_reduced, dR_reduced).cuda()
-    rb = RnnBlock(input_dim, embedding_dim, batch_size).cuda()
+    input_tensor = torch.randn(batch_size, cnn_channels, dR_reduced, LR_reduced)
+    rb = RnnBlock(input_dim, hparams.ref_embedding_dim, batch_size)
+    if test_params['cuda']:
+        input_tensor = input_tensor.cuda()
+        rb = rb.cuda()
+
     output = rb(input_tensor)
     assert output.shape == torch.Size([batch_size, embedding_dim])
 
 @test
-def _test_reference_encoder():
-    input_spec = torch.randn(hparams.batch_size, *hparams.input_dims).cuda()
-    re = ReferenceEncoder(hparams).cuda()
+def _test_reference_encoder(test_params):
+    input_spec = torch.randn(hparams.batch_size, *hparams.input_dims)
+    re = ReferenceEncoder(hparams)
+    if test_params['cuda']:
+        input_spec = input_spec.cuda()
+        re = re.cuda()
+
     embedding = re(input_spec)
     assert embedding.shape == torch.Size([hparams.batch_size, 128])
 
@@ -66,11 +81,19 @@ def _test_reference_encoder_real_spectrogram():
 
 
 ### RUN THE TESTS ###
-for name, fn in test_registry.items():
-    try:
-        fn()
-        print(colored("Passed test {}".format(name), 'green'))
-    except Exception :  # Here, pokemon exception handling is a-ok
-        print(colored("Tests failed in the execution of {}\n\n".format(name), 'red'),
-              traceback.format_exc())
-        exit(1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda', action='store_true')
+    namespace = parser.parse_args(sys.argv[1:])
+
+    test_params = defaultdict(lambda: False)
+    test_params.update(vars(namespace))
+
+    for name, fn in test_registry.items():
+        try:
+            fn(test_params)
+            print(colored("Passed test {}".format(name), 'green'))
+        except Exception :  # Here, pokemon exception handling is a-ok
+            print(colored("Tests failed in the execution of {}\n\n".format(name), 'red'),
+                traceback.format_exc())
+            exit(1)
